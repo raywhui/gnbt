c9c2f74767d26802ea98c648a0ed79cfbb4848c7
Raymond Hui <rwhui.tech@gmail.com>
2019-03-02 15:42:41 -0800

    adding frances code (modified)

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ const bot = new Discord.Client({
 const message = () => {
     bot.sendMessage({
         to: channelID,
-        message: `france test ${}`
+        message: `france test ${afdsf}`
     });
 }
 
@@ -30,7 +30,11 @@ bot.on('ready', (evt) => {
   logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-
+bot.setPresence({
+    game:{
+        name: "games with your heart."
+    }
+  });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
   // Our bot needs to know if it will execute a command
@@ -69,8 +73,11 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         });
                         break;
                 }
-            case 'shuan':
-
+            case 'whoami':
+                bot.sendMessage({
+                  to: channelID,
+                  message: "hahaaha u are " + "<@!" + userID + ">"
+                });
             break;
             // Just add any case commands if you want to..
     }
