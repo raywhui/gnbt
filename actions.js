d4254d233b91956b7f6a434412ff75caa14edc2a
Raymond Hui <rwhui.tech@gmail.com>
2019-03-02 16:27:03 -0800

    converted time into user consumable date time

--- actions.js
+++ actions.js
@@ -1,8 +1,13 @@
 const axios = require("axios");
 const auth = require("./auth.json");
 const upsReqBody = require("./requestBody.js");
+const moment = require("moment");
 
-// UPS is lame, needs to be done as post request
+/**
+ * API post request to ups (Poorly designed API as it should only be a pull request);
+ * @param {String} tracking - tracking ID
+ * @return {Object} Resolves with ups tracking response
+ */
 const packageTracking = (tracking) => {
   return axios.post(
     'https://onlinetools.ups.com/rest/Track',
@@ -16,21 +21,24 @@ const packageTracking = (tracking) => {
 }
 
 /**
- * Publish a notification. if `dismissAfter` is set, the notification will be
- * @param {Object} notif - Object containing
+ * Function to pull relevant data for easy user consumption
+ * @param {Object} activity - Object containing ups package tracking information
  */
 const searchPackage = (activity) => {
+  const dateTime = moment(
+    `${activity.Time}${activity.Date}`,
+    'hhmmssYYYYMMDD'
+  ).format('h:mm:ssa MMM DD, YYYY');
   switch (activity.Status.Type) {
     case 'D':
-      console.log(`${activity.Status.Description} at ${activity.ActivityLocation.Address.City}, ${activity.ActivityLocation.Address.StateProvinceCode} on ${activity.Time} ${activity.Date}`)
-      console.log("-----------------------------------")
+      console.log(`${activity.Status.Description} at ${activity.ActivityLocation.Address.City}, ${activity.ActivityLocation.Address.StateProvinceCode} on ${dateTime}`)
       break;
     case 'I':
-      console.log(`${activity.Status.Description} at ${activity.ActivityLocation.Address.City}, ${activity.ActivityLocation.Address.StateProvinceCode} on ${activity.Time} ${activity.Date}`)
+      console.log(`${activity.Status.Description} at ${activity.ActivityLocation.Address.City}, ${activity.ActivityLocation.Address.StateProvinceCode} on ${dateTime}`)
       console.log("-----------------------------------")
       break;
     case 'M':
-      console.log(`${activity.Status.Description} on ${activity.Time} ${activity.Date}`)
+      console.log(`${activity.Status.Description} on ${dateTime}`)
       console.log("-----------------------------------")
       break;
     default:
@@ -40,29 +48,29 @@ const searchPackage = (activity) => {
 };
 
 // V Define search function
-async function search() {
+async function search(arg3) {
   try {
     const data = await packageTracking(auth.test);
     const trackingData = data.data.TrackResponse.Shipment.Package.Activity;
-    
-    trackingData[0]
-    
-    trackingData.forEach((activity) => {
-      searchPackage(activity);
-    });
+    switch (arg3) {
+      case 'all':
+        for (activity of trackingData) {
+          await searchPackage(activity);
+        };
+        break;
+      default:
+        await searchPackage(trackingData[0]);
+        break;
+    };
   } catch(err) {
-    console.log('ERROR:', err);
-  }
-
-}
+    return `ERROR: ${err}`;
+  };
+};
 
 
 // Running search function
-search();
+// module.exports = search;
+search('all')
 
-// console.log(upsReqBody(
-//   auth.upsUser,
-//   auth.upsPass,
-//   auth.upsServiceAccessToken,
-//   auth.test
-// ))
+// console.log(moment('', '').format())
+// console.log(moment('15583720190211', 'hhmmssYYYYMMDD').format('h:mm:ssa MMM DD, YYYY'))
